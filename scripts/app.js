a7d41c678f6410e683e3ee39af3759b9b18b2db5
david robison <mrdavidrobison@gmail.com>
2017-10-09 17:31:47 -0600

    change id reference

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
   var d = new Date();
   var n = d.getFullYear();
-  $(".copyright-year").html('Copyright ' + ' © ' + n + ' Proskriptive All Rights Reserved.');
+  $("#copyright-year").html('Copyright ' + ' © ' + n + ' Proskriptive All Rights Reserved.');
 })
